fb76ac9db970a6fc2f9bc6a2bf2ec7a91a7a8745
Gilles Lima <gillestoim@gmail.com>
2021-01-17 11:36:18 -0300

    fix: update layout and js file

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 "use strict";
-import { me, links } from './data';
+import { me, links } from "./data";
 
 const DEFAULT_COLOR = "#585959";
 
-function socialButton({ text, link, color }) {
+function socialLink({ text, link, color }) {
   const element = document.createElement("li");
   element.innerHTML = `<a href="${link}" class="social-link" target="_blank">${text}</a>`;
   element.style.color = color ?? DEFAULT_COLOR;
@@ -12,36 +12,28 @@ function socialButton({ text, link, color }) {
 }
 
 function createLinks() {
-  const target = document.querySelector(".social");
   const element = document.createElement("ul");
 
-  links.forEach((item) => element.appendChild(socialButton(item)));
-  target.appendChild(element);
+  links.forEach((item) => element.appendChild(socialLink(item)));
+  document.querySelector(".social").appendChild(element);
 }
 
-function setName() {
-  const target = document.querySelector(".name");
-  target.innerHTML = me.name;
-}
-
-function setPosition() {
-  const target = document.querySelector(".position");
-  target.textContent = me.position;
+function setNameAndPosition() {
+  document.querySelector(".name").innerHTML = me.name;
+  document.querySelector(".position").textContent = me.position;
 }
 
 function setPicture() {
-  const target = document.querySelector(".picture");
   const element = document.createElement("img");
 
   element.setAttribute("src", me.picture);
   element.setAttribute("alt", me.name);
 
-  target.appendChild(element);
+  document.querySelector(".picture").appendChild(element);
 }
 
 (function () {
   setPicture();
-  setName();
-  setPosition();
+  setNameAndPosition();
   createLinks();
 })();
